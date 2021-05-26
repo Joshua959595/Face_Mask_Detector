3df7cda877c5cae9d7b03d52c5dffd92f69d76b7
Joshua959595 <wozhddl@hanyang.ac.kr>
2021-05-26 22:02:47 +0900

    Update data.py
    data part done

--- data.py
+++ data.py
@@ -68,14 +68,36 @@ def download_image(kind):
         print(kind,'이미지 다운로드 완료')
 
 # 점 간의 거리 계산
-def calcDistance(a, b):
+def calcDistance_p2p(a, b):
     return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+
+# 점&직선 간의 거리 계산
+def calcDistance_p2l(point, line_point1, line_point2):
+    if line_point1[0] == line_point2[0]:
+        return np.abs(point[0] - line_point1[0])
+    a = -(line_point1[1] - line_point2[1])/(line_point1[0] - line_point2[0])
+    b = 1
+    c = -a * line_point1[0] - b * line_point2[1]
+    return np.abs(a * point[0] + b * point[1] + c) / np.sqrt(a**2 + b**2)
+
+# 회전 점 계산
+def rotate_point(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + np.cos(angle) * (px - ox) - np.sin(angle) * (py - oy)
+    qy = oy + np.sin(angle) * (px - ox) + np.cos(angle) * (py - oy)
+    return qx, qy
 
 
 # 마스크 합성
 def mask_process(face_image_file_name):
     # 이미지 경로 생성
-    face_image_path = 'data/without_mask/'+face_image_file_name
+    face_image_path = 'data/without_mask/'+face_image_file_name+'.jpg'
     mask_image_path = 'data/mask.png'
 
     # 얼굴 영역 추출, 얼굴 랜드마크 추출
@@ -96,32 +118,87 @@ def mask_process(face_image_file_name):
         mask_width_ratio = 1.2
 
         # 마스크 높이 계산 (nose_bridge 2번째 점 & chin 9번째 점 간의 길이)
-        mask_height = round(calcDistance(face_landmark['nose_bridge'][1], face_landmark['chin'][8]))
+        mask_height = round(calcDistance_p2p(face_landmark['nose_bridge'][1], face_landmark['chin'][8]))
 
         # 마스크 좌/우 분할
-        mask_left = mask_image.crop((0, 0, round(mask_image.width/2), mask_image.height))
-        mask_right = mask_image.crop((round(mask_image.width/2), 0, mask_image.width, mask_image.height))
+        mask_left_image = mask_image.crop((0, 0, round(mask_image.width/2), mask_image.height))
+        mask_right_image = mask_image.crop((round(mask_image.width/2), 0, mask_image.width, mask_image.height))
 
-        # 왼쪽 얼굴 너비 계산
+        # 왼쪽 마스크 너비 계산
+        mask_left_width = round(calcDistance_p2l(face_landmark['chin'][0], face_landmark['nose_bridge'][0], face_landmark['chin'][8])*mask_width_ratio)
 
-        
         # 왼쪽 마스크 크기 조절
+        mask_left_image = mask_left_image.resize((mask_left_width, mask_height))
 
         # 오른쪽 얼굴 너비 계산
+        mask_right_width = round(calcDistance_p2l(face_landmark['chin'][16], face_landmark['nose_bridge'][0], face_landmark['chin'][8])*mask_width_ratio)
 
         # 오른쪽 마스크 크기 조절
+        mask_right_image = mask_right_image.resize((mask_right_width, mask_height))
 
-        # 좌/우 마스크 연결결
+        # 좌/우 마스크 연결
+        mask_image = Image.new('RGBA', (mask_left_width+mask_right_width, mask_height))
+        mask_image.paste(mask_left_image, (0, 0), mask_left_image)
+        mask_image.paste(mask_right_image, (mask_left_width, 0), mask_right_image)
 
         # 얼굴 회전 각도 계산
+        top_standard = face_landmark['nose_bridge'][0]
+        bottom_standard = face_landmark['chin'][8]
+        dx = bottom_standard[0] - top_standard[0]
+        dy = bottom_standard[1] - top_standard[1]
+        angleRadian = np.arctan2(dy, dx)
+        angleDegree = np.rad2deg(angleRadian)
+        angleUse = (90 - angleDegree + 360) % 360
 
         # 마스크 회전
+        mask_image = mask_image.rotate(angleUse, expand=True)
 
-        #
+        # 마스크 위치 계산
+        mask_radian = np.deg2rad(-angleUse)
 
+        center_x = (face_landmark['nose_bridge'][1][0] + face_landmark['chin'][8][0]) // 2
+        center_y = (face_landmark['nose_bridge'][1][1] + face_landmark['chin'][8][1]) // 2
+        p1 = rotate_point((center_x, center_y), (center_x - mask_left_width, center_y - mask_height // 2), mask_radian)
+        p2 = rotate_point((center_x, center_y), (center_x - mask_left_width, center_y + mask_height // 2), mask_radian)
+        p3 = rotate_point((center_x, center_y), (center_x + mask_left_width, center_y - mask_height // 2), mask_radian)
+        p4 = rotate_point((center_x, center_y), (center_x + mask_left_width, center_y + mask_height // 2), mask_radian)
+
+        box_x = round(min(p1[0], p2[0], p3[0], p4[0]))
+        box_y = round(min(p1[1], p2[1], p3[1], p4[1]))
+
+        # 마스크 합성
+        face_image.paste(mask_image, (box_x, box_y), mask_image)
+
+        face_count += 1
 
     # 결과 이미지 반환
     return face_image, face_count
 
-# 이터 생성
+# 데이터 생성
+def generate_data():
+    face_image_base_path = 'data/without_mask'
+    save_path = 'data/with_mask/'
+
+    face_image_file_names = os.listdir(face_image_base_path)
+
+    for i in range (len(face_image_file_names)):
+        face_image_file_name = face_image_file_names[i]
+
+        face_image, face_count = mask_process(face_image_file_name)
+
+        if face_count == 0:
+            os.remove(face_image_base_path + face_image_file_name)
+            print('얼굴 인식 실패 ('+str(i+1)+'/'+str(len(face_image_file_names))+'): '+face_image_file_name)
+        else:
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
+
+                face_image.save(save_path + face_image_file_name)
+                print('마스크 합성 중 ('+str(i+1)+'/'+str(len(face_image_file_names)) +'): '+face_image_file_name)
+
+    print('마스크 합성 완료')
+
+# 작동
+if __name__ == '__main__':
+    mask_process('7')[0].show()
 
